6a5bc9816224e4aa05cb2139e8e6ff0306442ac3
vard05 <a.a.arkhipov05@gmail.com>
2024-11-05 14:01:50 +0300

    обновил

--- java/lab5/IPValidator.java
+++ java/lab5/IPValidator.java
@@ -1,20 +1,23 @@
 import java.util.regex.*;
 
 public class IPValidator {
-    public static boolean isValidIP(String ip) {
-        String simplePattern = "^([0-9]{1,3}\\.){3}[0-9]{1,3}$";
-        if (!ip.matches(simplePattern)) return false;
-        
-        String[] parts = ip.split("\\.");
-        for (String part : parts) {
-            int number = Integer.parseInt(part);
-            if (number < 0 || number > 255) return false;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
-        System.out.println(isValidIP("192.168.1.1")); // true
-        System.out.println(isValidIP("256.100.50.25")); // false
+        System.out.println(testIP("192.168.1.1"));     // true
+        System.out.println(testIP("256.100.50.25"));   // false
+        System.out.println(testIP("000.168.1.1"));   // false
+    }
+
+    public static String testIP(String ip) {
+        Pattern pattern = Pattern.compile(
+            "^((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)$"
+        );
+        Matcher matcher = pattern.matcher(ip);
+
+        if (matcher.matches()) {
+            return "IP-адрес " + ip + " корректен!";
+        } else {
+            return "Некорректный IP-адрес: " + ip;
+        }
     }
 }
